585a56ce9b8f6c07833a4492306666823e4eba47
Chaffexd <shanechaffe@gmail.com>
2025-01-01 15:20:06 +0000

    move map into component

---
+++ components/GoogleMap.js
@@ -0,0 +1,45 @@
+import { AdvancedMarker, APIProvider, Map } from "@vis.gl/react-google-maps";
+import React from "react";
+
+const GoogleMap = ({ selectedCafe, setSelectedCafe, visitedCafes }) => {
+  return (
+    <>
+      <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+        <Map
+          style={{ width: "100%", height: "500px" }}
+          defaultCenter={{
+            lat: 45.559322,
+            lng: -2.924647,
+          }}
+          defaultZoom={2.1}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+          zoomControl={true}
+          mapTypeId="roadmap"
+          mapId="Coffee-Locations"
+        >
+          {visitedCafes.map((location, index) => (
+            <AdvancedMarker
+              key={index}
+              position={{
+                lat: location.lat,
+                lng: location.lon,
+              }}
+              onClick={() => setSelectedCafe(location)}
+            />
+          ))}
+          {selectedCafe && (
+            <InfoWindow
+              position={{ lat: selectedCafe.lat, lng: selectedCafe.lon }}
+              onCloseClick={() => setSelectedCafe(null)}
+            >
+              <h3 className="font-bold">{selectedCafe.title}</h3>
+            </InfoWindow>
+          )}
+        </Map>
+      </APIProvider>
+    </>
+  );
+};
+
+export default GoogleMap;
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,4 @@
+import GoogleMap from "@/components/GoogleMap";
 import SeoData from "@/components/SeoData";
 import { client } from "@/lib/contentful";
 import {
@@ -52,41 +53,12 @@ const AboutPage = ({ aboutPageProps }) => {
         So far we have been to{" "}
         <span className="font-bold">{aboutPageProps.length}</span> places!{" "}
       </h1>
-      <section className=" my-12 w-full">
-        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-          <Map
-            style={{ width: "100%", height: "500px" }}
-            defaultCenter={{
-              lat: 45.559322,
-              lng: -2.924647,
-            }}
-            defaultZoom={2.1}
-            gestureHandling={"greedy"}
-            disableDefaultUI={true}
-            zoomControl={true}
-            mapTypeId="roadmap"
-            mapId="Coffee-Locations"
-          >
-            {visitedCafes.map((location, index) => (
-              <AdvancedMarker
-                key={index}
-                position={{
-                  lat: location.lat,
-                  lng: location.lon,
-                }}
-                onClick={() => setSelectedCafe(location)}
-              />
-            ))}
-            {selectedCafe && (
-              <InfoWindow
-                position={{ lat: selectedCafe.lat, lng: selectedCafe.lon }}
-                onCloseClick={() => setSelectedCafe(null)}
-              >
-                <h3 className="font-bold">{selectedCafe.title}</h3>
-              </InfoWindow>
-            )}
-          </Map>
-        </APIProvider>
+      <section className="mb-12 mt-10 w-full">
+        <GoogleMap
+          selectedCafe={selectedCafe}
+          setSelectedCafe={setSelectedCafe}
+          visitedCafes={visitedCafes}
+        />
       </section>
     </article>
   );
