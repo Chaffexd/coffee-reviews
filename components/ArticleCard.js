20030cd230128797559226de5f9e9bba8b481a8f
Chaffexd <shanechaffe@gmail.com>
2025-08-21 15:35:20 +0100

    adding ninetailed

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -1,15 +1,25 @@
+import { useNinetailed } from "@ninetailed/experience.js-next";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const ArticleCard = ({ article }) => {
-  const { articleIntroSnippet, pageTitle, articlePreviewImage, pagePath, slug } =
-    article.fields;
-    
+  const { track } = useNinetailed();
+  const {
+    articleIntroSnippet,
+    pageTitle,
+    articlePreviewImage,
+    pagePath,
+    slug,
+  } = article.fields;
+
   return (
     <Link
       href={`${pagePath.fields.slug}/${slug}`}
       className="bg-coffee-medium flex flex-col items-center w-[330px] my-8 rounded-t-xl hover:-translate-y-2 transition-transform transform"
+      onClick={() => {
+        track(pageTitle);
+      }}
     >
       <Image
         src={`https:${articlePreviewImage.fields.image.fields.file.url}`}
@@ -19,8 +29,14 @@ const ArticleCard = ({ article }) => {
         className="w-full rounded-t-xl max-h-56 object-fit h-full"
       />
       <div className="p-8 text-center ">
-        <h3 className="text-coffee-creamLight font-bold text-2xl">{pageTitle}</h3>
-        <p className="text-coffee-creamDark text-lg">{articleIntroSnippet.length > 90 ? `${articleIntroSnippet.substring(0, 90)}...` : articleIntroSnippet}</p>
+        <h3 className="text-coffee-creamLight font-bold text-2xl">
+          {pageTitle}
+        </h3>
+        <p className="text-coffee-creamDark text-lg">
+          {articleIntroSnippet.length > 90
+            ? `${articleIntroSnippet.substring(0, 90)}...`
+            : articleIntroSnippet}
+        </p>
       </div>
     </Link>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import { NinetailedProvider } from "@ninetailed/experience.js-next";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <NinetailedProvider
+        clientId="84b3bf8f-867a-4648-88bb-a85d5ba448ef"
+        >
+        <Component {...pageProps} />
+      </NinetailedProvider>
     </Layout>
   );
 }
