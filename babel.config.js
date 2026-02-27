e36982e7d65617aea8c5630d867eae80bbcb0517
Chaffexd <shanechaffe@gmail.com>
2026-02-27 13:47:05 +0000

    change babel

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-    presets: [
-      '@babel/preset-env', // For modern JavaScript
-      '@babel/preset-react', // For JSX and React
-      'next/babel'
-    ],
-  };
-  
+  presets: ["next/babel"],
+};
